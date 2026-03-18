b24a5015e0521f7026ca233fd12d9cb252d92b5d
anmol11379-web <anmol.25bai11379@vitbhopal.ac.in>
2026-03-18 23:24:11 +0530

    Update QRcodeGenerator.py

--- QRcodeGenerator.py
+++ QRcodeGenerator.py
@@ -1,1 +1,17 @@
 #QR CODE GENERATOR
+
+import qrcode
+
+def create_qr(data):
+    qr = qrcode.QRCode(version=1, box_size=10, border=4)
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("my_qr.png")
+
+
+data = input("Enter data to encode in QR: ")
+create_qr(data)
+
+print("QR generated successfully")
